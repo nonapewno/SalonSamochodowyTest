52ba33455a3706f26f0844d03be308f5d96d3c18
NoNaPeWnO <nonapewno77@gmail.com>
2019-01-19 12:04:58 +0100

    Tekst w metodzie

--- SalonSamochodowy/DataProcessing.cs
+++ SalonSamochodowy/DataProcessing.cs
@@ -10,6 +10,7 @@ namespace SalonSamochodowy
     {
         public Samochod PobierzSamochodZNajwiekszymWyposazeniem(List<Samochod> samochody)
         {
+            Console.WriteLine("Samochód z największym wyposażeniem: ");
             return null;
         }
 
